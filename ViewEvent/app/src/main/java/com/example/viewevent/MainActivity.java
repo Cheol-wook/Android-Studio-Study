89f5631185040551926fac23c7b786bbbe9024e5
Cheol-wook <loky123@naver.com>
2022-06-24 15:40:29 +0900

    add Object

--- ViewEvent/app/src/main/java/com/example/viewevent/MainActivity.java
+++ ViewEvent/app/src/main/java/com/example/viewevent/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.viewevent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Myview myview = new Myview(this);
+        setContentView(myview);
         setContentView(R.layout.activity_main);
+    }
+
+    class Myview extends View {
+        Myview(Context context){
+            super(context);
+            setBackgroundColor(Color.GREEN);
+        }
+
+        public boolean onTouchEvent(MotionEvent event){
+            if(event.getAction() == MotionEvent.ACTION_DOWN){
+                Toast.makeText(MainActivity.this, "View 클래스를 상속받아 만든 이벤트 처리",
+                         Toast.LENGTH_SHORT).show();
+            }
+            return false;
+        }
     }
 }
